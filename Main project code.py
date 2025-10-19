11de4f754908aa482235f23b79a8730b004358ac
hugo piper <hp3300p@gre.ac.uk>
2025-10-19 10:58:32 +0100

    Testing to confirm github repo is working.

--- Main project code.py
+++ Main project code.py
@@ -3,3 +3,5 @@ This will be the main python file where we submit our code.
 
 TODO Decide what tasks we will all individually do.
 '''
+
+print("If you can see this, the GitHub repository is working.")
